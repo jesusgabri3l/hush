2e7f199fc073657ccebf46e20f0fc9bde266be14
Jesús Gabriel <jesus15202009@gmail.com>
2021-04-12 00:05:18 -0500

    Images

--- src/components/layouts/products/CardProduct.vue
+++ src/components/layouts/products/CardProduct.vue
@@ -3,8 +3,8 @@
     <div class="cardProduct" :class="action ? '' : 'cardProduct--lined'">
         <img :src="`/storage/${product.foto}`" class="cardProduct__image img-fluid">
         <div class="d-flex mt-4">
-            <img :src="`/storage/${product.foto}`" class="img-fluid cardProduct__image--sm">
-            <img :src="`/storage/${product.foto}`" class="img-fluid ml-4 cardProduct__image--sm">
+            <img :src="`storage/${product.foto}`" class="img-fluid cardProduct__image--sm">
+            <img :src="`storage/${product.foto}`" class="img-fluid ml-4 cardProduct__image--sm">
         </div>
         <p class="text cardProduct__text mt-4">{{product.nombre}}</p>
         <small class="cardProduct__price text--primary mt-4">${{product.precio}}</small>
